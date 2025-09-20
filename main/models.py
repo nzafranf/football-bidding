657235a1acee94706dad2384b1037b9c3c48b00d
nzafranf <nzfadhilll@gmail.com>
2025-09-20 22:07:42 +0700

    tugas 4 done

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 import uuid
@@ -11,7 +12,8 @@ class Product(models.Model):
         ('fan', 'Fan'),
         ('misc', 'Miscellanous'),
     ]
-
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     product_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     price = models.IntegerField(default=0)
--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from main.views import register
+from main.views import login_user
+from main.views import logout_user
 from main.views import show_main, create_product, show_product
 
 app_name = 'main'
@@ -7,4 +10,7 @@ urlpatterns = [
     path('', show_main, name='show_main'),
     path('create-product/', create_product, name='create_product'),
     path('product/<str:id>/', show_product, name='show_product'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
